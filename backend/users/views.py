7606dfec03c65222fff492a45e31efcd2734d48d
punit <punitsinha495@gmail.com>
2025-09-30 21:42:21 +0530

    added parser

--- backend/users/views.py
+++ backend/users/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
 
 User = get_user_model()
 
@@ -52,6 +53,7 @@ class listApiView(generics.ListAPIView):
 class EditProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    parser_classes = [MultiPartParser, FormParser]
 
     def get_object(self):
         return self.request.user
