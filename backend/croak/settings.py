e62f9fe1b216d0a27dbeb6c560831d2c9751516f
punit <punitsinha495@gmail.com>
2025-10-03 23:34:43 +0530

    removed

--- backend/croak/settings.py
+++ backend/croak/settings.py
@@ -183,12 +183,5 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "punitsinha495@gmail.com"  
-EMAIL_HOST_PASSWORD = os.getenv("APP_PASSWORD") 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
